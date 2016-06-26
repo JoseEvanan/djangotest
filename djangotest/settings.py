1b31951be4fa879f697f0d13455fd43b7174d7be
Jose Evanan <jose.evanan@gmail.com>
2016-06-26 03:47:12 -0500

    data

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -104,8 +104,8 @@ DATABASES = {
     }
 }
 
-#DATABASES['default'] =  dj_database_url.config()
-#SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+DATABASES['default'] =  dj_database_url.config()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators

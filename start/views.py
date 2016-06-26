961fa84b51b9857a291459a6eced3f476bb67b6e
Jose Evanan <jose.evanan@gmail.com>
2016-06-26 04:13:06 -0500

    print

--- start/views.py
+++ start/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
 from .models import data
 def index(request):
-    print "llego a index"
     datas = data.objects.all()
     return render(request,"start/home.html",{"data":datas})
 def save(request):
-    #http://127.0.0.1:8000/save/?value=12
     value = str(request.GET['value'])
     dat = data(value=value)
     dat.save()
